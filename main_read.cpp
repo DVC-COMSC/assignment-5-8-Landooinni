64b79ff45ffc6784414b1468cd78e96b9b89eca9
Landooinni <97581425+Landooinni@users.noreply.github.com>
2024-03-08 20:39:01 -0800

    idk

--- main_read.cpp
+++ main_read.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-
+ifstream ifs("student.txt");
 int main()
 {
-	/***************************************************
-	 * Code your program here
-	 ************************************************** */
+	int n;
+	ifs>>n;
+	for (size_t i = 0; i < n; i++)
+	{
+	string name;
+	int sum,s1,s2;
+	float avg;
+
+	ifs.open("student.txt",std::ifstream::in);
+	ifs>>name;
+	ifs>>s1;
+	ifs>>s2;
+	ifs.close();
+	for ( size_t j=0; j<2; j++)
+	{
+	sum=s2+s1;
+	avg=sum/2;
+	cout<<name<<" "<<sum<<" "<<avg<<"\n";
+	}
 }
--- main_write.cpp
+++ main_write.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-
+ifstream ifs("student.txt");
 int main()
 {
-   /***************************************************
-    * Code your program here
-    ************************************************** */
+int N;
+
+int score1,score2;
+   string n;
+   
+    cin>>N;
+    ifs.open("student.txt",std::ifstream::in);
+    cout<<N<<"\n";
+    ifs.close();
+    for (size_t i = 0; i < N; i++)
+    {
+        cin >>n>> score1>>score2;
+        ifs.open("student.txt");
+        cout<<n<<"\n"<<score1<<"\n"<<score2<<"\n";
+        ifs.close();
+    }
+    
+    
+
 }
